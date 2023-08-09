7c373e043d531f3532d1a5ccc23f1b36e3f96bc5
daiki1020 <badkyo.1020@gmail.com>
2023-08-09 11:56:49 +0900

    基本機能実装完了

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -1,16 +1,17 @@
 export default (io, socket) => {
     // 入室メッセージをクライアントに送信する
     socket.on("enterEvent", (data) => {
-        io.sockets.emit("enterEvent", data)
+        socket.broadcast.emit("enterEvent", data + "さんが入室しました。")
     })
 
     // 退室メッセージをクライアントに送信する
     socket.on("exitEvent", (data) => {
-        io.sockets.emit("exitEvent", data)
+        socket.broadcast.emit("exitEvent", data + "さんが退室しました。")
     })
 
     // 投稿メッセージを送信する
     socket.on("publishEvent", (data) => {
-        io.sockets.emit("publishEvent", data)
+        let message = data.userName + "さん：" + data.chatContent
+        io.sockets.emit("publishEvent", message)
     })
 }
--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -24,22 +24,30 @@ onMounted(() => {
 // #region browser event handler
 // 投稿メッセージをサーバに送信する
 const onPublish = () => {
+  let data = {}
+  data.userName = userName.value
+  data.chatContent = chatContent.value
+  socket.emit("publishEvent", data)
 
   // 入力欄を初期化
+  chatContent.value = ""
 
   return false
 }
 
 // 退室メッセージをサーバに送信する
 const onExit = () => {
-
+  socket.emit("exitEvent", userName.value)
 }
 
 // メモを画面上に表示する
 const onMemo = () => {
   // メモの内容を表示
+  let memo = userName.value + "さんのメモ：" + chatContent.value
+  chatList.unshift(memo)
 
   // 入力欄を初期化
+  chatContent.value = ""
 
   return false
 }
@@ -48,17 +56,17 @@ const onMemo = () => {
 // #region socket event handler
 // サーバから受信した入室メッセージ画面上に表示する
 const onReceiveEnter = (data) => {
-  onReceiveEnter(data)
+  chatList.unshift(data)
 }
 
 // サーバから受信した退室メッセージを受け取り画面上に表示する
 const onReceiveExit = (data) => {
-  onReceiveExit(data)
+  chatList.unshift(data)
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
 const onReceivePublish = (data) => {
-  onReceivePublish(data)
+  chatList.unshift(data)
 }
 // #endregion
 
@@ -67,17 +75,17 @@ const onReceivePublish = (data) => {
 const registSocketEvent = () => {
   // 入室イベントを受け取ったら実行
   socket.on("enterEvent", (data) => {
-
+    onReceiveEnter(data)
   })
 
   // 退室イベントを受け取ったら実行
   socket.on("exitEvent", (data) => {
-
+    onReceiveExit(data)
   })
 
   // 投稿イベントを受け取ったら実行
   socket.on("publishEvent", (data) => {
-
+    onReceivePublish(data)
   })
 }
 // #endregion
@@ -88,10 +96,10 @@ const registSocketEvent = () => {
     <h1 class="text-h3 font-weight-medium">Vue.js Chat チャットルーム</h1>
     <div class="mt-10">
       <p>ログインユーザ：{{ userName }}さん</p>
-      <textarea variant="outlined" placeholder="投稿文を入力してください" rows="4" class="area"></textarea>
+      <textarea variant="outlined" placeholder="投稿文を入力してください" rows="4" class="area" v-model="chatContent"></textarea>
       <div class="mt-5">
-        <button class="button-normal">投稿</button>
-        <button class="button-normal util-ml-8px">メモ</button>
+        <button class="button-normal" @click="onPublish">投稿</button>
+        <button class="button-normal util-ml-8px" @click="onMemo">メモ</button>
       </div>
       <div class="mt-5" v-if="chatList.length !== 0">
         <ul>
